28ca793e9dbcda4aa7f0d8e7f706eef95e73aee2
Olga Kent <olgakent2@gmail.com>
2017-09-05 16:56:21 -0400

    Updated problems from chapter 5

--- chapter05.js
+++ chapter05.js
@@ -21,12 +21,33 @@ ancestry.forEach(function(person) {
 // Problem 1: Flattening
 function flatten(arrays) {
   // Your code here
+  // reduce takes a function with previous and current values
+  return(arrays.reduce(function(prev, curr) {
+    // return a new array - previous concatenated with current
+    return prev.concat(curr);
+  }, []));
 }
 
 // Problem 2: Mother-child age difference
 /* This must return the average age difference instead of printing it */
 function averageMomChildAgeDiff() {
   // Your code here
+  var ageDiffs = [];
+  var peopleMap = {};
+
+  ancestry.forEach((person) => {
+    peopleMap[person.name] = person;
+  });
+
+  ancestry.forEach((child) => {
+    var mom = peopleMap[child.mother];
+    if(mom) {
+      ageDiffs.push(child.born - mom.born);
+    }
+  });
+
+  return average(ageDiffs);
+
 }
 
 // Problem 3: Historical life expectancy
@@ -35,6 +56,24 @@ function averageMomChildAgeDiff() {
  */
 function averageAgeByCentury() {
   // Your code here
+  var century = {};
+
+  // test whether century was already known for each person
+  ancestry.forEach((person) => {
+    let index = Math.ceil(person.died/100);
+    // if not, add an array for it
+    if(!(index in century)) {
+      century[index] = [];
+    }
+    // add the persons age to the array for the proper century
+    century[index].push(person.died - person.born);
+  });
+
+  for (var index in century) {
+    century[index] = Number(average(century[index]));
+  }
+
+  return century;
 }
 
 
